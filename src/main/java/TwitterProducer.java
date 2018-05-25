0d953a4915769b073d6b26933988dfa0af92e656
Tanmay Mehra <mehra.tanmay@gmail.com>
2018-05-25 12:45:51 +0530

    producer to add messages to kafka

--- src/main/java/TwitterProducer.java
+++ src/main/java/TwitterProducer.java
@@ -1,13 +1,64 @@
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class TwitterProducer {
 
     private static final String TOPIC = "twitterStream";
+    private static int count = 0;
 
     public static void run(String consumerKey, String consumerSecret,
                            String token, String secret) throws InterruptedException {
-        Properties properties = new Properties();
-        properties.put("metadata.broker.list", "localhost:9092");
-        properties.put("serializer.class", "kafka.serializer.StringEncoder");
+        Properties prop = new Properties();
+        prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(prop);
+
+        BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
+        StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
+        // add some track terms
+        endpoint.trackTerms(Lists.newArrayList("twitterapi",
+                "#FIFA18"));
+
+        Authentication auth = new OAuth1(consumerKey, consumerSecret,
+                token, secret);
+
+        Client client = new ClientBuilder().hosts(Constants.STREAM_HOST)
+                .endpoint(endpoint).authentication(auth)
+                .processor(new StringDelimitedProcessor(queue)).build();
+
+        client.connect();
+
+        for (int i = 0; i < 1000; i++) {
+            producer.send(new ProducerRecord<String, String>(TOPIC, queue.take()));
+            System.out.println("Produced record: " + count++);
+        }
+        producer.close();
+        client.stop();
+    }
+
+    public static void main(String[] args) {
+        try {
+            TwitterProducer.run(args[0], args[1], args[2], args[3]);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
